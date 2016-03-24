ac6337537d4e95981ad42c0629f2074c07a46883
Shaw <shawlevin@gmail.com>
2016-03-23 23:06:04 -0400

    added performance checks

---
+++ Tutorials/Performance.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Tutorials
+{
+    public static class Performance
+    {
+        public static void Loops()
+        {
+            int counter = 10000000;
+            Console.WriteLine("Loop compares with counter: " + counter);
+            List<Instrument> ins = new List<Instrument>();
+
+            //Setup
+            for (int i = 0; i < counter; i++)
+            {
+                ins.Add(new Instrument());
+            }
+
+            Stopwatch sw = new Stopwatch();
+
+            //ForEach
+            sw.Start();
+            foreach (Instrument i in ins)
+            {
+                i.Id = 1;
+            }
+            sw.Stop();
+            Console.WriteLine("ForEach: " + sw.ElapsedMilliseconds);
+
+            //For Loop
+            sw.Restart();
+            for (int i = 0; i < ins.Count; i++)
+            {
+                ins[i].Id = 1;
+            }
+            sw.Stop();
+            Console.WriteLine("For 0 to Counter: " + sw.ElapsedMilliseconds);
+
+            //Loop counting down instead of up.
+            sw.Restart();
+            for (int i = ins.Count - 1; i != 0; i--)
+            {
+                ins[i].Id = 1;
+            }
+            sw.Stop();
+            Console.WriteLine("For Counter to 0: " + sw.ElapsedMilliseconds);
+
+            //Loop counting down instead of up.
+            sw.Restart();
+            for (int i = ins.Count - 1; i != 0; i--)
+            {
+                ins[i]._id = 1;
+            }
+            sw.Stop();
+            Console.WriteLine("For Set Field: " + sw.ElapsedMilliseconds);
+        }
+
+        public static void Strings()
+        {
+            Stopwatch sw = new Stopwatch();
+            String result = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            int counter = 100000;
+
+            Console.WriteLine("String manipulations with counter: " + counter);
+
+            sw.Restart();
+            for (int i = 0; i < counter; i++)
+            {
+                sb.Append(" ");
+            }
+            sw.Stop();
+            Console.WriteLine("StringBuilder: " +sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < counter; i++)
+            {
+                result += " ";
+            }
+            sw.Stop();
+            Console.WriteLine("String: " + sw.ElapsedMilliseconds);
+
+
+        }
+
+        public static void IntTryParse()
+        {
+            Stopwatch sw = new Stopwatch();
+            int result = 0;
+            string parse = "Shaw";
+            int counter = 100000;
+
+            Console.WriteLine("Int parsing with counter: " + counter);
+
+            sw.Restart();
+            for (int i = 0; i < counter; i++)
+            {
+                Int32.TryParse(parse, out result);
+            }
+            sw.Stop();
+            Console.WriteLine("TryParse: " + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < counter; i++)
+            {
+                try
+                {
+                    result = Int32.Parse(parse);
+                }
+                catch (Exception ex)
+                {
+                    result = 0;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine("Parse: " + sw.ElapsedMilliseconds);
+        }
+
+        public static void DictionaryContainsKey()
+        {
+            Stopwatch sw = new Stopwatch();
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            int counter = 100000;
+
+            Console.WriteLine("Dictionary key checking with counter: " + counter);
+
+            sw.Restart();
+            for (int i = 0; i < counter; i++)
+            {
+                if (dictionary.ContainsKey(counter))
+                {
+                    Console.WriteLine(dictionary[counter]);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine("ContainsKey: " + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < counter; i++)
+            {
+                try
+                {
+                    Console.WriteLine(dictionary[counter]);
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+            sw.Stop();
+            Console.WriteLine("TryCatch: " + sw.ElapsedMilliseconds);
+        }
+    }
+}
--- Tutorials/Program.cs
+++ Tutorials/Program.cs
@@ -12,112 +12,17 @@ namespace Tutorials
             //Casting();
 
             //RefAndValueTypes();
-            HackerRankMilitaryTime("07:05:45PM");
+            //HackerRank.MilitaryTime("07:05:45PM");
+            //HackerRank.Staircase();
+            //HackerRank.Sherlock(66317); //-1
 
-            Console.WriteLine(HackerRankSherlock(66317)); //-1
+            Performance.DictionaryContainsKey();
+            Performance.IntTryParse();
+            Performance.Loops();
+            Performance.Strings();
         }
 
-        private static string Nums(int threes, int fives)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < fives; i++)
-            {
-                sb.Append("5");
-            }
-            for (int i = fives; i < (fives + threes); i++)
-            {
-                sb.Append("3");
-            }
 
-            return sb.ToString();
-        }
-
-        private static string HackerRankSherlock(int digits)
-        {
-            //Console.Write(digits + ": ");
-            if (digits < 3)
-            {
-                return "-1";
-            }
-            if (digits % 3 == 0)
-            {
-                return Nums(0, digits);
-            }
-
-            int factor = digits / 3;
-            int remainder = 0;
-
-            for (int i = factor; i > 0; i--)
-            {
-                remainder = digits - (3 * i);
-                if (remainder % 5 == 0)
-                {
-                    return Nums(remainder, 3 * i);
-                }
-            }
-            if (digits % 5 == 0)
-            {
-                return Nums(digits, 0);
-            }
-
-            return "-1";
-        }
-
-        private static void HackerRankMilitaryTime(string input)
-        {
-            DateTime d = DateTime.Parse(input);
-
-            //19:05:45
-            String hours = "", minutes = "", seconds = "";
-            if (d.Hour > 9)
-            {
-                hours = d.Hour.ToString();
-            }
-            else
-            {
-                hours = "0" + d.Hour.ToString();
-            }
-
-            if (d.Minute > 9)
-            {
-                minutes = d.Minute.ToString();
-            }
-            else
-            {
-                minutes = "0" + d.Minute.ToString();
-            }
-
-            if (d.Second > 9)
-            {
-                seconds = d.Second.ToString();
-            }
-            else
-            {
-                seconds = "0" + d.Second.ToString();
-            }
-            Console.WriteLine(hours + ":" + minutes + ":" + seconds);
-        }
-
-        private static void HackerRankStaircase()
-        {
-            int n = 5;
-            char[] list = new char[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                list[i] = ' ';
-            }
-
-            for (int i = n - 1; i >= 0; i--)
-            {
-                list[i] = '#';
-                for (int x = 0; x < list.Length; x++)
-                {
-                    Console.Write(list[x]);
-                }
-                Console.WriteLine("");
-            }
-        }
 
         /// <summary>
         /// Objects are reference types and are passed by reference in C#.
